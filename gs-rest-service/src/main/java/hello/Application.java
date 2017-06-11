5733350b0e219db04328187b552b5f74a4d2dc49
nahidhossain <nahidhossain@192.168.0.104>
2017-06-11 22:39:08 +0600

    changed

--- gs-rest-service/src/main/java/hello/Application.java
+++ gs-rest-service/src/main/java/hello/Application.java
@@ -1,9 +1,12 @@
 package hello;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,36 +14,28 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 @SpringBootApplication
-public class Application {
-
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class Application extends WebSecurityConfigurerAdapter {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-    
-    
-    @Value("${allowed.user}")
-    private String ALLOWED_USER;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().authenticated().and().x509().subjectPrincipalRegex("CN=(.*?)(?:,|$)").userDetailsService(userDetailsService());
+    }
 
     @Bean
-    public UserDetailsService userDetailsService () {
-
+    public UserDetailsService userDetailsService() {
         return new UserDetailsService() {
-
             @Override
-            public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException {
-                if (username.equals(ALLOWED_USER)) {
-                    final User user = new User(username, "", AuthorityUtils.createAuthorityList("ROLE_SSL_USER"));
-                    return user;
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                if (username.equals("cid")) {
+                    return new User(username, "", AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER"));
                 }
-                return null;
+                throw new UsernameNotFoundException("User not found!");
             }
         };
-    }	
-    
-    
-    
-    
-    
-    
-    
+    }
 }
--- gs-rest-service/src/main/java/hello/GreetingController.java
+++ gs-rest-service/src/main/java/hello/GreetingController.java
@@ -1,6 +1,13 @@
 package hello;
 
+import java.security.Principal;
 import java.util.concurrent.atomic.AtomicLong;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +17,19 @@ public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
+    
+	
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
 
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+    @PreAuthorize("hasAuthority('ROLE_USER')")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name,Principal principal) {
+    
+    	
+    	UserDetails currentUser = (UserDetails) ((Authentication) principal).getPrincipal();
+    	
+    	log.info("User is :"+ currentUser.getUsername());
+    	
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
